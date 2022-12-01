49c1ac87ef780abb10f3ad5131f6bf92d9425e81
Megha Mallela <meghamallela9@gmail.com>
2022-12-01 10:56:15 +0530

    Update HelloWorld.java
    Changes !!

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
       public static void main(String args[]){
             //This line will print "Hello World".
             System.out.println("Hello World.");
+            System.out.println("Change the code");
       }
 }
